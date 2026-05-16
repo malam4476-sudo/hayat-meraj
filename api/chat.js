8fa715a500d21e6b8f91ef880f8e077b886d785e
malam4476-sudo <malam4476@gmail.com>
2026-05-16 12:10:59 +0530

    Update chat.js
    api

--- api/chat.js
+++ api/chat.js
@@ -23,7 +23,7 @@ User: ${userText}`;
         "Authorization": `Bearer ${process.env.GROQ_API_KEY}`
       },
       body: JSON.stringify({
-        model: "llama3-8b-8192",
+       model: "llama-3.1-8b-instant",
         messages: [
           {
             role: "user",
